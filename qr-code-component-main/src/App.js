d5143b2f5ae4905e07f56b5956f50fd0befa377b
PixieMgt <pixiemoortgat@outlook.com>
2023-03-31 20:34:17 +0200

    completed challenge

--- qr-code-component-main/src/App.js
+++ qr-code-component-main/src/App.js
@@ -1,13 +1,17 @@
+import QrCode from './images/image-qr-code.png'
+import './App.css';
+
 function App() {
   return (
     <>
-      Improve your front-end skills by building projects
-
-      Scan the QR code to visit Frontend Mentor and take your coding skills to the next level
-
+      <div className='container'>
+        <img src={QrCode} alt='QR Code' />
+        <p className='title'>Improve your front-end skills by building projects</p>
+        <p className='instructions'>Scan the QR code to visit Frontend Mentor and take your coding skills to the next level</p>
+      </div >
       <div class="attribution">
         Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>.
-        Coded by <a href="#">Your Name Here</a>.
+        Coded by <a href="#">Pixie Moortgat</a>.
       </div>
     </>
   );
